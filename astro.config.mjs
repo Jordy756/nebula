6fb78ea8c1479cf4d0464007b6c02185af9a85ec
Jordy756 <jordycastro1756@gmail.com>
2026-05-21 23:40:20 -0600

    feat(4G-001): add Google Fonts configuration and update layout components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,37 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  fonts: [
+    {
+      name: 'Space Grotesk',
+      cssVariable: '--font-space-grotesk',
+      provider: fontProviders.google(),
+      display: 'swap',
+      subsets: ['latin'],
+      styles: ['normal'],
+      weights: [300],
+      formats: ['woff2'],
+      unicodeRange: ['U+0000-00FF'],
+      fallbacks: ['sans-serif'],
+    },
+    {
+      name: 'Space Mono',
+      cssVariable: '--font-space-mono',
+      provider: fontProviders.google(),
+      display: 'swap',
+      subsets: ['latin'],
+      styles: ['normal'],
+      weights: [400, 700],
+      formats: ['woff2'],
+      unicodeRange: ['U+0000-00FF'],
+      fallbacks: ['monospace'],
+    },
+  ],
 });
